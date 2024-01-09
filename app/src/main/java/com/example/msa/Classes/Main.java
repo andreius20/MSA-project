76b45a5ee29f0397040b969223bb74f38800d1bb
andreius20 <andreiuspus@gmail.com>
2024-01-09 21:25:55 +0200

    Added Recipee List controller

--- app/src/main/java/com/example/msa/Classes/Main.java
+++ app/src/main/java/com/example/msa/Classes/Main.java
@@ -1,4 +1,4 @@
-package Classes;
+package com.example.msa.Classes;
 
 public class Main {
 
---
+++ app/src/main/java/com/example/msa/controllers/AddRecipeeController.java
@@ -0,0 +1,33 @@
+package com.example.msa.controllers;
+
+import com.example.msa.Classes.Lista;
+import com.example.msa.Classes.Reteta;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+
+
+@RestController
+@RequestMapping("/login")
+
+public class AddRecipeeController {
+
+    @GetMapping
+    public ArrayList<Reteta> getListData() {
+        Lista a = new Lista();
+        return a.getList();
+    }
+
+    @PostMapping
+    public void AddRecipeeToList(Reteta r) {
+        Lista a = new Lista();
+        a.AddRecipe(r);
+    }
+
+}
